7421fc6a1d7140dc071064fe7b672be3faa10af9
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2024-03-11 07:17:17 -0400

    Slider Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Header from './Components/Header'
+import Slider from './Components/Slider'
 
 function App() {  
 
   return (
     <div className="">
       <Header />
+      
+      <Slider />
     </div>
   )
 }
