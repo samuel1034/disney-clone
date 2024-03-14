0f6081899d95e4cf3525f57e82ce9f458cd8e70b
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2024-03-14 09:56:36 -0400

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Header from './Components/Header'
 import Slider from './Components/Slider'
 import ProductionHouse from './Components/ProductionHouse'
+import GenreMovieList from './Components/GenreMovieList'
+
 
 function App() {  
 
@@ -12,6 +14,10 @@ function App() {
       <Slider/>
 
       <ProductionHouse/>
+
+      <GenreMovieList/>
+
+        
       
     </div>
   )
--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -42,14 +42,18 @@ function ProductionHouse() {
 
     ]
   return (
-    <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16'>
-         {productionHouseList.map((item)=>(
+    <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16 '>
+        {productionHouseList.map((item)=>(
             <div className='border-[2px] border-gray-600
-            rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer shadow-xl shadow-gray-800'>
-                
-                <video src={item.video} className=' absolute top-0 rounded-md z-0 opacity-0 hover:opacity-50' autoPlay loop> playsInline</video>   
-                <img src={item.image} className='w-full z-[1] '/>
-                             
+            rounded-lg hover:scale-110 transition-all duration-300
+            ease-in-out cursor-pointer relative shadow-xl 
+            shadow-gray-800
+            '>
+                 <video src={item.video} autoPlay loop playsInline muted 
+            className='absolute z-0  top-0 rounded-md 
+            opacity-0 hover:opacity-50'/> 
+                <img src={item.image} className='w-full z-[1] opacity-100' /> 
+               
             </div>
         ))}
     </div>
